7682bc13fb3c87e1125c336302dfd4421bdf8c9c
Scott Brisson <scott.brisson@gmail.com>
2021-02-07 17:08:57 -0600

    initial push

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import cuddlePuddle from './stinny.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={cuddlePuddle} className="App-Gif" alt="Cuddle Puddle" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
